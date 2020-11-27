dd25bf549063bb38a5f613bf2a70ac53d2d1091a
James Lovejoy <james.lovejoy2@gmail.com>
2020-11-27 13:03:01 -0600

    added resolver functions and added created mongodb connect file

---
+++ src/dao/index.ts
@@ -0,0 +1,31 @@
+import { MongoClient, Db } from "mongodb";
+export * from "./types";
+
+const {DB_USER, DB_PASSWORD, DB_CLUSTER } = process.env
+let url: string = '';
+
+if(!DB_USER) {
+  throw new Error('Please define the DB_USER env variable')
+} else if(!DB_PASSWORD) {
+  throw new Error('Please define the DB_PASSWORD env variable')
+} else if(!DB_CLUSTER) {
+  throw new Error('Please define the DB_CLUSTER env variable')
+} else {
+url = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}.mongodb.net/<dbname>?retryWrites=true&w=majority`;
+}
+
+export let client: MongoClient;
+export let database: Db;
+
+export const connect = async (): Promise<Db> => {
+  if (!database) {
+    console.info(`Connecting to database ${url}`);
+    client = await MongoClient.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    database = client.db("main");
+  }
+
+  return database;
+};
--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,48 +1,64 @@
 import { Resolvers, TodoMvc } from "./types";
+import { connect } from "../dao";
+import { TodoMvcDbObject } from "../dao/types";
+import { ObjectID } from "mongodb";
 
-let store: TodoMvc[] = [
-  {
-    todoId: "1",
-    completed: false,
-    description: "foo",
-  },
-  {
-    todoId: "2",
-    completed: false,
-    description: "bar",
-  },
-];
+const dbPromise = connect();
+
+const getCollection = async () => {
+  const db = await dbPromise;
+  return db.collection<TodoMvcDbObject>("todos");
+};
+
+const fromDbObject = (dbObject: TodoMvcDbObject): TodoMvc => ({
+  todoId: dbObject._id.toHexString(),
+  completed: dbObject.completed,
+  description: dbObject.description,
+});
 
 const resolvers: Resolvers = {
   Query: {
-    allTodos: () => store,
-    Todo: (_: any, { todoId }) =>
-      store.find((d) => d.todoId === todoId),
+    allTodos: async () => {
+      const collection = await getCollection();
+      return await collection.find().map(fromDbObject).toArray();
+    },
+    Todo: async (_: any, { todoId }) => {
+      const collection = await getCollection();
+      const dbObject = await collection.findOne({
+        _id: ObjectID.createFromHexString(todoId),
+      });
+      return fromDbObject(dbObject);
+    },
   },
   Mutation: {
-    createTodo: (_: any, { description }) => {
-      const newTodo: TodoMvc = {
-        todoId: Math.floor(Math.random() * 10000).toString(),
+    createTodo: async (_: any, { description }) => {
+      const data: Omit<TodoMvcDbObject, "_id"> = {
         description,
-        completed: false
-      }
+        completed: false,
+      };
 
-      store = store.concat(newTodo);
-      return newTodo;
+      const collection = await getCollection();
+      const document = await collection.insertOne(data);
+      return fromDbObject({
+        ...data,
+        _id: document.insertedId,
+      });
     },
-    updateTodo: (_: any, { todoId, data }) => {
-      const todo = store.find(d => d.todoId === todoId);
+    updateTodo: async (_: any, { todoId, data }) => {
+      const collection = await getCollection();
+      const result = await collection.findOneAndUpdate(
+        {
+          _id: ObjectID.createFromHexString(todoId),
+        },
+        { $set: data },
+        {
+          returnOriginal: false,
+        }
+      );
 
-      if (todo) {
-        store = store.filter(d => d.todoId !== todoId).concat({
-          ...todo,
-          ...data
-        });
-      }
-
-      return todo;
-    }
-  }
+      return fromDbObject(result.value);
+    },
+  },
 };
 
 export default resolvers;
