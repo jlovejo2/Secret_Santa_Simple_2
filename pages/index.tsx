5371e981cbd823c1203f95d1968682d9b5dfd5f7
James Lovejoy <james.lovejoy2@gmail.com>
2020-11-27 08:47:23 -0600

    added an index page query to get all todos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,28 @@
-import { TodoMvc } from "../src/graphql/types";
-import { useState } from "react";
+import { useIndexQuery } from "../src/graphql/types";
+import { gql } from "@apollo/client";
+import Todo from "../components/todo";
+import { useState, ChangeEvent } from "react";
+
+gql`
+  query Index {
+    allTodos {
+      todoId
+    }
+  }
+`;
 
 const Index = () => {
-  const [todos] = useState([
-    {
-      todoId: "1",
-      completed: false,
-      description: "foo",
-    },
-  ] as TodoMvc[]);
-  const todoElements = todos.map((t) => (
-    <tr key={t.todoId}>
-      <td>
-        <input type="checkbox" checked={t.completed}></input>
-      </td>
-      <td>{t.description}</td>
-    </tr>
+  const { data, loading } = useIndexQuery();
+
+  const allTodos = data?.allTodos
+    ?.slice()
+    .sort((a, b) => a.todoId.localeCompare(b.todoId));
+
+  const todoElements = allTodos?.map((t) => (
+    <Todo todoId={t.todoId} key={t.todoId} />
   ));
-  return todos.length > 0 ? (
+
+  return loading ? null : todoElements.length > 0 ? (
     <table>
       <tbody>{todoElements}</tbody>
     </table>
