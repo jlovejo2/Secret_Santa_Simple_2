0df7a8c4d2a5e281684fe050eb8c4705038e54df
James Lovejoy <james.lovejoy2@gmail.com>
2020-11-27 17:33:40 -0600

    styled member submit form

--- components/GroupForm/GroupForm.tsx
+++ components/GroupForm/GroupForm.tsx
@@ -23,31 +23,33 @@ const GroupForm = (props: Props) => {
 //   let content = <td colSpan={2}>Loading ...</td>;
 
   return (
+      <Fragment>
        <form>
             <div>
-                <label>
-                    First Name: 
-                    <input type='text' name='firstName' />
+                <label className='block'>
+                    <span className='text-gray-800'>First Name:</span> 
+                    <input type='text' name='firstName' placeholder='First Name' className='form-input mt-1 block w-full border-solid border-2 border-black-300' />
                 </label>
             </div>
             <div>
-               <label>
-                    Last Name: 
-                    <input type='text' name='lastName' />
+               <label className='block'>
+                    <span className='text-gray-800'>Last Name:</span> 
+                    <input type='text' name='lastName' placeholder='Last Name' className='form-input mt-1 block w-full border-solid border-2 border-black-300'/>
                 </label>
             </div>
             <div>
-               <label>
-                    Email: 
-                    <input type='text' name='email' />
+               <label className='block'>
+                    <span className='text-gray-800'>Email:</span> 
+                    <input type='text' name='email' placeholder='Email' className='form-input mt-1 block w-full border-solid border-2 border-black-300'/>
                 </label>
             </div>
             <div>
-                <button type='submit' >
+                <button type='submit' className="btn-primary mt-2 transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-green-700 hover:bg-purple-900 text-white font-normal py-2 px-4 mr-1 rounded" >
                     Submit
                 </button>
             </div>
        </form>
+       </Fragment>
   );
 };
 
