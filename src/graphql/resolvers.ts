582793c207e409980996907d043af7ac453bf73d
James Lovejoy <james.lovejoy2@gmail.com>
2020-11-27 11:22:07 -0600

    added mutations to schema.graphql and resolvers.ts for creating and updating a todo

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,6 +1,6 @@
 import { Resolvers, TodoMvc } from "./types";
 
-const store: TodoMvc[] = [
+let store: TodoMvc[] = [
   {
     todoId: "1",
     completed: false,
@@ -19,6 +19,30 @@ const resolvers: Resolvers = {
     Todo: (_: any, { todoId }) =>
       store.find((d) => d.todoId === todoId),
   },
+  Mutation: {
+    createTodo: (_: any, { description }) => {
+      const newTodo: TodoMvc = {
+        todoId: Math.floor(Math.random() * 10000).toString(),
+        description,
+        completed: false
+      }
+
+      store = store.concat(newTodo);
+      return newTodo;
+    },
+    updateTodo: (_: any, { todoId, data }) => {
+      const todo = store.find(d => d.todoId === todoId);
+
+      if (todo) {
+        store = store.filter(d => d.todoId !== todoId).concat({
+          ...todo,
+          ...data
+        });
+      }
+
+      return todo;
+    }
+  }
 };
 
 export default resolvers;
