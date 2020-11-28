7a2775df55b1e9badfe29e912cae0490e36b3588
James Lovejoy <james.lovejoy2@gmail.com>
2020-11-27 20:14:13 -0600

    added user type to schema.graphql ran codegen generate

--- src/dao/types.ts
+++ src/dao/types.ts
@@ -1,62 +1,76 @@
 export type Maybe<T> = T | null;
 export type Exact<T extends { [key: string]: unknown }> = {
-  [K in keyof T]: T[K];
+	[K in keyof T]: T[K];
 };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> &
-  { [SubKey in K]?: Maybe<T[SubKey]> };
+	{ [SubKey in K]?: Maybe<T[SubKey]> };
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> &
-  { [SubKey in K]: Maybe<T[SubKey]> };
+	{ [SubKey in K]: Maybe<T[SubKey]> };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: string;
-  String: string;
-  Boolean: boolean;
-  Int: number;
-  Float: number;
+	ID: string;
+	String: string;
+	Boolean: boolean;
+	Int: number;
+	Float: number;
 };
 
 export type Query = {
-  allTodos: Array<TodoMvc>;
-  Todo?: Maybe<TodoMvc>;
+	allTodos: Array<TodoMvc>;
+	Todo?: Maybe<TodoMvc>;
+	allUsers: Array<User>;
 };
 
 export type QueryTodoArgs = {
-  todoId: Scalars["ID"];
+	todoId: Scalars['ID'];
 };
 
 export type Mutation = {
-  createTodo: TodoMvc;
-  updateTodo?: Maybe<TodoMvc>;
+	createTodo: TodoMvc;
+	updateTodo?: Maybe<TodoMvc>;
 };
 
 export type MutationCreateTodoArgs = {
-  description: Scalars["String"];
+	description: Scalars['String'];
 };
 
 export type MutationUpdateTodoArgs = {
-  todoId: Scalars["ID"];
-  data: UpdateTodoInput;
+	todoId: Scalars['ID'];
+	data: UpdateTodoInput;
 };
 
 export type UpdateTodoInput = {
-  description?: Maybe<Scalars["String"]>;
-  completed?: Maybe<Scalars["Boolean"]>;
+	description?: Maybe<Scalars['String']>;
+	completed?: Maybe<Scalars['Boolean']>;
 };
 
 export type TodoMvc = {
-  todoId: Scalars["ID"];
-  completed: Scalars["Boolean"];
-  description: Scalars["String"];
+	todoId: Scalars['ID'];
+	completed: Scalars['Boolean'];
+	description: Scalars['String'];
+};
+
+export type User = {
+	userId: Scalars['ID'];
+	first_name: Scalars['String'];
+	last_name: Scalars['String'];
+	email: Scalars['String'];
 };
 
 export type AdditionalEntityFields = {
-  path?: Maybe<Scalars["String"]>;
-  type?: Maybe<Scalars["String"]>;
+	path?: Maybe<Scalars['String']>;
+	type?: Maybe<Scalars['String']>;
 };
 
-import { ObjectID } from "mongodb";
+import { ObjectID } from 'mongodb';
 export type TodoMvcDbObject = {
-  _id: ObjectID;
-  completed: boolean;
-  description: string;
+	_id: ObjectID;
+	completed: boolean;
+	description: string;
+};
+
+export type UserDbObject = {
+	first_name: string;
+	last_name: string;
+	email: string;
 };
--- src/graphql/types.tsx
+++ src/graphql/types.tsx
@@ -1,247 +1,266 @@
-import { GraphQLResolveInfo } from "graphql";
-import { gql } from "@apollo/client";
-import * as ApolloReactCommon from "@apollo/client";
-import * as ApolloReactHooks from "@apollo/client";
+import { GraphQLResolveInfo } from 'graphql';
+import { gql } from '@apollo/client';
+import * as ApolloReactCommon from '@apollo/client';
+import * as ApolloReactHooks from '@apollo/client';
 export type Maybe<T> = T | null;
 export type Exact<T extends { [key: string]: unknown }> = {
-  [K in keyof T]: T[K];
+	[K in keyof T]: T[K];
 };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> &
-  { [SubKey in K]?: Maybe<T[SubKey]> };
+	{ [SubKey in K]?: Maybe<T[SubKey]> };
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> &
-  { [SubKey in K]: Maybe<T[SubKey]> };
+	{ [SubKey in K]: Maybe<T[SubKey]> };
 export type RequireFields<T, K extends keyof T> = {
-  [X in Exclude<keyof T, K>]?: T[X];
+	[X in Exclude<keyof T, K>]?: T[X];
 } &
-  { [P in K]-?: NonNullable<T[P]> };
+	{ [P in K]-?: NonNullable<T[P]> };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: string;
-  String: string;
-  Boolean: boolean;
-  Int: number;
-  Float: number;
+	ID: string;
+	String: string;
+	Boolean: boolean;
+	Int: number;
+	Float: number;
 };
 
 export type Query = {
-  allTodos: Array<TodoMvc>;
-  Todo?: Maybe<TodoMvc>;
+	allTodos: Array<TodoMvc>;
+	Todo?: Maybe<TodoMvc>;
+	allUsers: Array<User>;
 };
 
 export type QueryTodoArgs = {
-  todoId: Scalars["ID"];
+	todoId: Scalars['ID'];
 };
 
 export type Mutation = {
-  createTodo: TodoMvc;
-  updateTodo?: Maybe<TodoMvc>;
+	createTodo: TodoMvc;
+	updateTodo?: Maybe<TodoMvc>;
 };
 
 export type MutationCreateTodoArgs = {
-  description: Scalars["String"];
+	description: Scalars['String'];
 };
 
 export type MutationUpdateTodoArgs = {
-  todoId: Scalars["ID"];
-  data: UpdateTodoInput;
+	todoId: Scalars['ID'];
+	data: UpdateTodoInput;
 };
 
 export type UpdateTodoInput = {
-  description?: Maybe<Scalars["String"]>;
-  completed?: Maybe<Scalars["Boolean"]>;
+	description?: Maybe<Scalars['String']>;
+	completed?: Maybe<Scalars['Boolean']>;
 };
 
 export type TodoMvc = {
-  todoId: Scalars["ID"];
-  completed: Scalars["Boolean"];
-  description: Scalars["String"];
+	todoId: Scalars['ID'];
+	completed: Scalars['Boolean'];
+	description: Scalars['String'];
+};
+
+export type User = {
+	userId: Scalars['ID'];
+	first_name: Scalars['String'];
+	last_name: Scalars['String'];
+	email: Scalars['String'];
 };
 
 export type ResolverTypeWrapper<T> = Promise<T> | T;
 
 export type LegacyStitchingResolver<TResult, TParent, TContext, TArgs> = {
-  fragment: string;
-  resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
+	fragment: string;
+	resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
 };
 
 export type NewStitchingResolver<TResult, TParent, TContext, TArgs> = {
-  selectionSet: string;
-  resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
+	selectionSet: string;
+	resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
 };
 export type StitchingResolver<TResult, TParent, TContext, TArgs> =
-  | LegacyStitchingResolver<TResult, TParent, TContext, TArgs>
-  | NewStitchingResolver<TResult, TParent, TContext, TArgs>;
+	| LegacyStitchingResolver<TResult, TParent, TContext, TArgs>
+	| NewStitchingResolver<TResult, TParent, TContext, TArgs>;
 export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> =
-  | ResolverFn<TResult, TParent, TContext, TArgs>
-  | StitchingResolver<TResult, TParent, TContext, TArgs>;
+	| ResolverFn<TResult, TParent, TContext, TArgs>
+	| StitchingResolver<TResult, TParent, TContext, TArgs>;
 
 export type ResolverFn<TResult, TParent, TContext, TArgs> = (
-  parent: TParent,
-  args: TArgs,
-  context: TContext,
-  info: GraphQLResolveInfo
+	parent: TParent,
+	args: TArgs,
+	context: TContext,
+	info: GraphQLResolveInfo
 ) => Promise<TResult> | TResult;
 
 export type SubscriptionSubscribeFn<TResult, TParent, TContext, TArgs> = (
-  parent: TParent,
-  args: TArgs,
-  context: TContext,
-  info: GraphQLResolveInfo
+	parent: TParent,
+	args: TArgs,
+	context: TContext,
+	info: GraphQLResolveInfo
 ) => AsyncIterator<TResult> | Promise<AsyncIterator<TResult>>;
 
 export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
-  parent: TParent,
-  args: TArgs,
-  context: TContext,
-  info: GraphQLResolveInfo
+	parent: TParent,
+	args: TArgs,
+	context: TContext,
+	info: GraphQLResolveInfo
 ) => TResult | Promise<TResult>;
 
 export interface SubscriptionSubscriberObject<
-  TResult,
-  TKey extends string,
-  TParent,
-  TContext,
-  TArgs
+	TResult,
+	TKey extends string,
+	TParent,
+	TContext,
+	TArgs
 > {
-  subscribe: SubscriptionSubscribeFn<
-    { [key in TKey]: TResult },
-    TParent,
-    TContext,
-    TArgs
-  >;
-  resolve?: SubscriptionResolveFn<
-    TResult,
-    { [key in TKey]: TResult },
-    TContext,
-    TArgs
-  >;
+	subscribe: SubscriptionSubscribeFn<
+		{ [key in TKey]: TResult },
+		TParent,
+		TContext,
+		TArgs
+	>;
+	resolve?: SubscriptionResolveFn<
+		TResult,
+		{ [key in TKey]: TResult },
+		TContext,
+		TArgs
+	>;
 }
 
 export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
-  subscribe: SubscriptionSubscribeFn<any, TParent, TContext, TArgs>;
-  resolve: SubscriptionResolveFn<TResult, any, TContext, TArgs>;
+	subscribe: SubscriptionSubscribeFn<any, TParent, TContext, TArgs>;
+	resolve: SubscriptionResolveFn<TResult, any, TContext, TArgs>;
 }
 
 export type SubscriptionObject<
-  TResult,
-  TKey extends string,
-  TParent,
-  TContext,
-  TArgs
+	TResult,
+	TKey extends string,
+	TParent,
+	TContext,
+	TArgs
 > =
-  | SubscriptionSubscriberObject<TResult, TKey, TParent, TContext, TArgs>
-  | SubscriptionResolverObject<TResult, TParent, TContext, TArgs>;
+	| SubscriptionSubscriberObject<TResult, TKey, TParent, TContext, TArgs>
+	| SubscriptionResolverObject<TResult, TParent, TContext, TArgs>;
 
 export type SubscriptionResolver<
-  TResult,
-  TKey extends string,
-  TParent = {},
-  TContext = {},
-  TArgs = {}
+	TResult,
+	TKey extends string,
+	TParent = {},
+	TContext = {},
+	TArgs = {}
 > =
-  | ((
-      ...args: any[]
-    ) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
-  | SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>;
+	| ((
+			...args: any[]
+	  ) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
+	| SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>;
 
 export type TypeResolveFn<TTypes, TParent = {}, TContext = {}> = (
-  parent: TParent,
-  context: TContext,
-  info: GraphQLResolveInfo
+	parent: TParent,
+	context: TContext,
+	info: GraphQLResolveInfo
 ) => Maybe<TTypes> | Promise<Maybe<TTypes>>;
 
 export type IsTypeOfResolverFn<T = {}, TContext = {}> = (
-  obj: T,
-  context: TContext,
-  info: GraphQLResolveInfo
+	obj: T,
+	context: TContext,
+	info: GraphQLResolveInfo
 ) => boolean | Promise<boolean>;
 
 export type NextResolverFn<T> = () => Promise<T>;
 
 export type DirectiveResolverFn<
-  TResult = {},
-  TParent = {},
-  TContext = {},
-  TArgs = {}
+	TResult = {},
+	TParent = {},
+	TContext = {},
+	TArgs = {}
 > = (
-  next: NextResolverFn<TResult>,
-  parent: TParent,
-  args: TArgs,
-  context: TContext,
-  info: GraphQLResolveInfo
+	next: NextResolverFn<TResult>,
+	parent: TParent,
+	args: TArgs,
+	context: TContext,
+	info: GraphQLResolveInfo
 ) => TResult | Promise<TResult>;
 
 /** Mapping between all available schema types and the resolvers types */
 export type ResolversTypes = {
-  Query: ResolverTypeWrapper<{}>;
-  ID: ResolverTypeWrapper<Scalars["ID"]>;
-  Mutation: ResolverTypeWrapper<{}>;
-  String: ResolverTypeWrapper<Scalars["String"]>;
-  UpdateTodoInput: UpdateTodoInput;
-  Boolean: ResolverTypeWrapper<Scalars["Boolean"]>;
-  TodoMVC: ResolverTypeWrapper<TodoMvc>;
+	Query: ResolverTypeWrapper<{}>;
+	ID: ResolverTypeWrapper<Scalars['ID']>;
+	Mutation: ResolverTypeWrapper<{}>;
+	String: ResolverTypeWrapper<Scalars['String']>;
+	UpdateTodoInput: UpdateTodoInput;
+	Boolean: ResolverTypeWrapper<Scalars['Boolean']>;
+	TodoMVC: ResolverTypeWrapper<TodoMvc>;
+	User: ResolverTypeWrapper<User>;
 };
 
 /** Mapping between all available schema types and the resolvers parents */
 export type ResolversParentTypes = {
-  Query: {};
-  ID: Scalars["ID"];
-  Mutation: {};
-  String: Scalars["String"];
-  UpdateTodoInput: UpdateTodoInput;
-  Boolean: Scalars["Boolean"];
-  TodoMVC: TodoMvc;
+	Query: {};
+	ID: Scalars['ID'];
+	Mutation: {};
+	String: Scalars['String'];
+	UpdateTodoInput: UpdateTodoInput;
+	Boolean: Scalars['Boolean'];
+	TodoMVC: TodoMvc;
+	User: User;
 };
 
 export type QueryResolvers<
-  ContextType = any,
-  ParentType extends ResolversParentTypes["Query"] = ResolversParentTypes["Query"]
+	ContextType = any,
+	ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']
 > = {
-  allTodos?: Resolver<
-    Array<ResolversTypes["TodoMVC"]>,
-    ParentType,
-    ContextType
-  >;
-  Todo?: Resolver<
-    Maybe<ResolversTypes["TodoMVC"]>,
-    ParentType,
-    ContextType,
-    RequireFields<QueryTodoArgs, "todoId">
-  >;
+	allTodos?: Resolver<Array<ResolversTypes['TodoMVC']>, ParentType, ContextType>;
+	Todo?: Resolver<
+		Maybe<ResolversTypes['TodoMVC']>,
+		ParentType,
+		ContextType,
+		RequireFields<QueryTodoArgs, 'todoId'>
+	>;
+	allUsers?: Resolver<Array<ResolversTypes['User']>, ParentType, ContextType>;
 };
 
 export type MutationResolvers<
-  ContextType = any,
-  ParentType extends ResolversParentTypes["Mutation"] = ResolversParentTypes["Mutation"]
+	ContextType = any,
+	ParentType extends ResolversParentTypes['Mutation'] = ResolversParentTypes['Mutation']
 > = {
-  createTodo?: Resolver<
-    ResolversTypes["TodoMVC"],
-    ParentType,
-    ContextType,
-    RequireFields<MutationCreateTodoArgs, "description">
-  >;
-  updateTodo?: Resolver<
-    Maybe<ResolversTypes["TodoMVC"]>,
-    ParentType,
-    ContextType,
-    RequireFields<MutationUpdateTodoArgs, "todoId" | "data">
-  >;
+	createTodo?: Resolver<
+		ResolversTypes['TodoMVC'],
+		ParentType,
+		ContextType,
+		RequireFields<MutationCreateTodoArgs, 'description'>
+	>;
+	updateTodo?: Resolver<
+		Maybe<ResolversTypes['TodoMVC']>,
+		ParentType,
+		ContextType,
+		RequireFields<MutationUpdateTodoArgs, 'todoId' | 'data'>
+	>;
 };
 
 export type TodoMvcResolvers<
-  ContextType = any,
-  ParentType extends ResolversParentTypes["TodoMVC"] = ResolversParentTypes["TodoMVC"]
+	ContextType = any,
+	ParentType extends ResolversParentTypes['TodoMVC'] = ResolversParentTypes['TodoMVC']
 > = {
-  todoId?: Resolver<ResolversTypes["ID"], ParentType, ContextType>;
-  completed?: Resolver<ResolversTypes["Boolean"], ParentType, ContextType>;
-  description?: Resolver<ResolversTypes["String"], ParentType, ContextType>;
-  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+	todoId?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+	completed?: Resolver<ResolversTypes['Boolean'], ParentType, ContextType>;
+	description?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+	__isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type UserResolvers<
+	ContextType = any,
+	ParentType extends ResolversParentTypes['User'] = ResolversParentTypes['User']
+> = {
+	userId?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+	first_name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+	last_name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+	email?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+	__isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 };
 
 export type Resolvers<ContextType = any> = {
-  Query?: QueryResolvers<ContextType>;
-  Mutation?: MutationResolvers<ContextType>;
-  TodoMVC?: TodoMvcResolvers<ContextType>;
+	Query?: QueryResolvers<ContextType>;
+	Mutation?: MutationResolvers<ContextType>;
+	TodoMVC?: TodoMvcResolvers<ContextType>;
+	User?: UserResolvers<ContextType>;
 };
 
 /**
@@ -251,33 +270,33 @@ export type Resolvers<ContextType = any> = {
 export type IResolvers<ContextType = any> = Resolvers<ContextType>;
 
 export type TodoQueryVariables = Exact<{
-  todoId: Scalars["ID"];
+	todoId: Scalars['ID'];
 }>;
 
 export type TodoQuery = {
-  Todo?: Maybe<Pick<TodoMvc, "description" | "completed">>;
+	Todo?: Maybe<Pick<TodoMvc, 'description' | 'completed'>>;
 };
 
 export type UpdateTodoMutationVariables = Exact<{
-  todoId: Scalars["ID"];
-  data: UpdateTodoInput;
+	todoId: Scalars['ID'];
+	data: UpdateTodoInput;
 }>;
 
 export type UpdateTodoMutation = {
-  updateTodo?: Maybe<Pick<TodoMvc, "description" | "completed">>;
+	updateTodo?: Maybe<Pick<TodoMvc, 'description' | 'completed'>>;
 };
 
 export type IndexQueryVariables = Exact<{ [key: string]: never }>;
 
-export type IndexQuery = { allTodos: Array<Pick<TodoMvc, "todoId">> };
+export type IndexQuery = { allTodos: Array<Pick<TodoMvc, 'todoId'>> };
 
 export const TodoDocument = gql`
-  query Todo($todoId: ID!) {
-    Todo(todoId: $todoId) {
-      description
-      completed
-    }
-  }
+	query Todo($todoId: ID!) {
+		Todo(todoId: $todoId) {
+			description
+			completed
+		}
+	}
 `;
 
 /**
@@ -297,41 +316,41 @@ export const TodoDocument = gql`
  * });
  */
 export function useTodoQuery(
-  baseOptions: ApolloReactHooks.QueryHookOptions<TodoQuery, TodoQueryVariables>
+	baseOptions: ApolloReactHooks.QueryHookOptions<TodoQuery, TodoQueryVariables>
 ) {
-  return ApolloReactHooks.useQuery<TodoQuery, TodoQueryVariables>(
-    TodoDocument,
-    baseOptions
-  );
+	return ApolloReactHooks.useQuery<TodoQuery, TodoQueryVariables>(
+		TodoDocument,
+		baseOptions
+	);
 }
 export function useTodoLazyQuery(
-  baseOptions?: ApolloReactHooks.LazyQueryHookOptions<
-    TodoQuery,
-    TodoQueryVariables
-  >
+	baseOptions?: ApolloReactHooks.LazyQueryHookOptions<
+		TodoQuery,
+		TodoQueryVariables
+	>
 ) {
-  return ApolloReactHooks.useLazyQuery<TodoQuery, TodoQueryVariables>(
-    TodoDocument,
-    baseOptions
-  );
+	return ApolloReactHooks.useLazyQuery<TodoQuery, TodoQueryVariables>(
+		TodoDocument,
+		baseOptions
+	);
 }
 export type TodoQueryHookResult = ReturnType<typeof useTodoQuery>;
 export type TodoLazyQueryHookResult = ReturnType<typeof useTodoLazyQuery>;
 export type TodoQueryResult = ApolloReactCommon.QueryResult<
-  TodoQuery,
-  TodoQueryVariables
+	TodoQuery,
+	TodoQueryVariables
 >;
 export const UpdateTodoDocument = gql`
-  mutation UpdateTodo($todoId: ID!, $data: UpdateTodoInput!) {
-    updateTodo(todoId: $todoId, data: $data) {
-      description
-      completed
-    }
-  }
+	mutation UpdateTodo($todoId: ID!, $data: UpdateTodoInput!) {
+		updateTodo(todoId: $todoId, data: $data) {
+			description
+			completed
+		}
+	}
 `;
 export type UpdateTodoMutationFn = ApolloReactCommon.MutationFunction<
-  UpdateTodoMutation,
-  UpdateTodoMutationVariables
+	UpdateTodoMutation,
+	UpdateTodoMutationVariables
 >;
 
 /**
@@ -353,30 +372,30 @@ export type UpdateTodoMutationFn = ApolloReactCommon.MutationFunction<
  * });
  */
 export function useUpdateTodoMutation(
-  baseOptions?: ApolloReactHooks.MutationHookOptions<
-    UpdateTodoMutation,
-    UpdateTodoMutationVariables
-  >
+	baseOptions?: ApolloReactHooks.MutationHookOptions<
+		UpdateTodoMutation,
+		UpdateTodoMutationVariables
+	>
 ) {
-  return ApolloReactHooks.useMutation<
-    UpdateTodoMutation,
-    UpdateTodoMutationVariables
-  >(UpdateTodoDocument, baseOptions);
+	return ApolloReactHooks.useMutation<
+		UpdateTodoMutation,
+		UpdateTodoMutationVariables
+	>(UpdateTodoDocument, baseOptions);
 }
 export type UpdateTodoMutationHookResult = ReturnType<
-  typeof useUpdateTodoMutation
+	typeof useUpdateTodoMutation
 >;
 export type UpdateTodoMutationResult = ApolloReactCommon.MutationResult<UpdateTodoMutation>;
 export type UpdateTodoMutationOptions = ApolloReactCommon.BaseMutationOptions<
-  UpdateTodoMutation,
-  UpdateTodoMutationVariables
+	UpdateTodoMutation,
+	UpdateTodoMutationVariables
 >;
 export const IndexDocument = gql`
-  query Index {
-    allTodos {
-      todoId
-    }
-  }
+	query Index {
+		allTodos {
+			todoId
+		}
+	}
 `;
 
 /**
@@ -395,30 +414,30 @@ export const IndexDocument = gql`
  * });
  */
 export function useIndexQuery(
-  baseOptions?: ApolloReactHooks.QueryHookOptions<
-    IndexQuery,
-    IndexQueryVariables
-  >
+	baseOptions?: ApolloReactHooks.QueryHookOptions<
+		IndexQuery,
+		IndexQueryVariables
+	>
 ) {
-  return ApolloReactHooks.useQuery<IndexQuery, IndexQueryVariables>(
-    IndexDocument,
-    baseOptions
-  );
+	return ApolloReactHooks.useQuery<IndexQuery, IndexQueryVariables>(
+		IndexDocument,
+		baseOptions
+	);
 }
 export function useIndexLazyQuery(
-  baseOptions?: ApolloReactHooks.LazyQueryHookOptions<
-    IndexQuery,
-    IndexQueryVariables
-  >
+	baseOptions?: ApolloReactHooks.LazyQueryHookOptions<
+		IndexQuery,
+		IndexQueryVariables
+	>
 ) {
-  return ApolloReactHooks.useLazyQuery<IndexQuery, IndexQueryVariables>(
-    IndexDocument,
-    baseOptions
-  );
+	return ApolloReactHooks.useLazyQuery<IndexQuery, IndexQueryVariables>(
+		IndexDocument,
+		baseOptions
+	);
 }
 export type IndexQueryHookResult = ReturnType<typeof useIndexQuery>;
 export type IndexLazyQueryHookResult = ReturnType<typeof useIndexLazyQuery>;
 export type IndexQueryResult = ApolloReactCommon.QueryResult<
-  IndexQuery,
-  IndexQueryVariables
+	IndexQuery,
+	IndexQueryVariables
 >;
