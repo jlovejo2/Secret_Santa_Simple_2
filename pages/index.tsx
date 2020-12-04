6f50869f2b8c3cee1c0a6ab6ac5819b6792102a6
James Lovejoy <james.lovejoy2@gmail.com>
2020-12-04 09:09:08 -0600

    added functionalit to index.tsx page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,57 +1,7 @@
-import { useIndexQuery } from "../src/graphql/types";
+import { CreateGroupInput, GroupMember, useIndexQuery } from "../src/graphql/types";
 import { gql } from "@apollo/client";
-import { Todo, GroupForm, GroupSummary, Navbar } from "../components";
+import { GroupCombined, Todo, Navbar } from "../components";
 import { useState, ChangeEvent, SyntheticEvent, useEffect, FormEvent } from "react";
-
-export type groupMember = {
-    first_name: String,
-    last_name: String,
-    email: String,
-    secret_pick?: Object;
-}
-
-const psuedoGroup = [
-    {
-        first_name: 'Eddie',
-        last_name: 'Lovejoy',
-        email: 'james.lovejoy2@gmail.com'
-    },
-    {
-        first_name: 'Kelly',
-        last_name: 'Phelan',
-        email: 'kelyphelan8@gmail.com'
-    },
-    {
-        first_name: 'Jane',
-        last_name: 'Watt',
-        email: 'jane@gmail.com'
-    },
-    {
-        first_name: 'Darius',
-        last_name: 'Watt',
-        email: 'darius@gmail.com'
-    },    
-    {
-        first_name: 'Brian',
-        last_name: 'Phelan',
-        email: 'bphelan@gmail.com'
-    },
-    {
-        first_name: 'Gina',
-        last_name: 'Phelan',
-        email: 'gphelan@gmail.com'
-    },
-    {
-        first_name: 'Sharon',
-        last_name: 'Phelan',
-        email: 'phelan.sharon.l@gmail.com'
-    },
-    {
-        first_name: 'Tom',
-        last_name: 'Phelan',
-        email: 'phelan.thomas.j@gmail.com'
-    }
-]
 
 gql`
   query Index {
@@ -64,8 +14,6 @@ gql`
 const Index = () => {
     const { data, loading } = useIndexQuery();
     const [newTodoDescription, setNewTodoDescription] = useState("");
-    const [groupDetails, setGroupDetails ] = useState<groupMember[]>(psuedoGroup);
-    const [currentGroupMember, setCurrentGroupMember] = useState<groupMember>()
 
     const [todoIds, setTodoIds] = useState<string[]>();
   
@@ -87,31 +35,6 @@ const Index = () => {
   
     const onClickAddTodo = () => {};
   
-    const handleNewGroupMember = (e: FormEvent ) => {
-        e.preventDefault();
-
-        setGroupDetails([...groupDetails, currentGroupMember])
-    }
-
-    const handleChangeGroupForm = (e: ChangeEvent) => {
-
-        const fieldName = (e.currentTarget as HTMLInputElement).name
-        const fieldValue = (e.currentTarget as HTMLInputElement).value
-
-        switch (fieldName) {
-            case 'firstName':
-                setCurrentGroupMember({...currentGroupMember, first_name: fieldValue })
-                break;
-            case 'lastName':
-                setCurrentGroupMember({...currentGroupMember, last_name: fieldValue })
-                break;
-            case 'email':
-                setCurrentGroupMember({...currentGroupMember, email: fieldValue })
-                break;
-            default:
-                break;
-        }
-    }
 
     const todoElements = todoIds?.map((id) => <Todo todoId={id} key={id} />);
   
@@ -145,12 +68,7 @@ const Index = () => {
         </button>
             {body}
         </div>
-        <div className='col-start-3 col-span-2 justify-center'>
-            <GroupForm groupDetails={groupDetails} handleChangeGroupForm={handleChangeGroupForm} onSubmit={handleNewGroupMember}/>
-        </div>
-        <div className='col-start-2 col-span-4'>
-            <GroupSummary groupDetails={groupDetails} />
-        </div>
+            <GroupCombined />
         </div>
       </>
     );
