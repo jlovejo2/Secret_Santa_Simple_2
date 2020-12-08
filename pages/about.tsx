3cd3884e753f0d080c8317a8ff1465b5c0e77ec1
James Lovejoy <james.lovejoy2@gmail.com>
2020-12-08 14:48:29 -0600

    added signup component to about.tsx page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { SignUp } from '../components';
 
 const Index = () => {
 	return (
 		<>
 			<div>
 				<p> This is the about page </p>
+				<SignUp />
 			</div>
 		</>
 	);
