ebd5045824150d85a141d9e535b81a574c79b88a
James Lovejoy <james.lovejoy2@gmail.com>
2020-12-08 11:20:45 -0600

    removed unwanted comments in GroupSummary

--- components/GroupInfo/GroupSummary.tsx
+++ components/GroupInfo/GroupSummary.tsx
@@ -1,46 +1,40 @@
-import { gql } from "@apollo/client";
-import { GroupMember } from "../../src/graphql/types";
-import { useState, ChangeEvent, useEffect, Fragment, SetStateAction, Dispatch } from "react";
-import { TableHeader, TableRow } from "../Table";
+import { gql } from '@apollo/client';
+import { GroupMember } from '../../src/graphql/types';
+import {
+	useState,
+	ChangeEvent,
+	useEffect,
+	Fragment,
+	SetStateAction,
+	Dispatch
+} from 'react';
+import { TableHeader, TableRow } from '../Table';
 import { emailSender } from '../../src/dao/nodeMailer';
-import { CreateGroupInput } from "../../src/dao";
+import { CreateGroupInput } from '../../src/dao';
 
 interface Props {
-    groupDetails: GroupMember[] | CreateGroupInput[],
+	groupDetails: GroupMember[] | CreateGroupInput[];
 }
 
-
-
 const GroupSummary = (props: Props) => {
-    const { groupDetails } = props
+	const { groupDetails } = props;
 
-  return ( 
-    <Fragment>
-      { groupDetails ? (
-          <>
-            <table className='table-auto w-full text-center'>
-                  <TableHeader groupDetails={groupDetails} />
-                  <tbody>
-                    <TableRow groupDetails={groupDetails} />
-                  </tbody>
-            </table>
-            {/* <div>
-            {groupWithPicks ? (
-                <table className='table-auto w-full text-center'>
-                    <TableHeader groupDetails={groupWithPicks} />
-                    <tbody>
-                        <TableRow groupDetails={groupWithPicks} />
-                    </tbody>
-                </table>
-            ) : (
-            <div>No picks yet.</div>
-            ) }
-            </div> */}
-          </>
-      ) : (<div>Nothing to show.</div>)
-      }
-      </Fragment>
-  );
+	return (
+		<Fragment>
+			{groupDetails ? (
+				<>
+					<table className='table-auto w-full text-center'>
+						<TableHeader groupDetails={groupDetails} />
+						<tbody>
+							<TableRow groupDetails={groupDetails} />
+						</tbody>
+					</table>
+				</>
+			) : (
+				<div>Nothing to show.</div>
+			)}
+		</Fragment>
+	);
 };
 
 export default GroupSummary;
