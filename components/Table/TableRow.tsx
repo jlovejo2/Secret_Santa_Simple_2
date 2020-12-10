2048ccbc5225f2328f50b54b5c61eaf6d09e5da6
James Lovejoy <james.lovejoy2@gmail.com>
2020-12-09 19:06:25 -0600

    added edit and delete buttons to group member info

--- components/Table/TableRow.tsx
+++ components/Table/TableRow.tsx
@@ -1,36 +1,49 @@
 import React, { Fragment } from 'react';
 import { GroupMember } from '../../src/graphql/types';
 
-
 type TableRowProps = {
-    groupDetails: GroupMember[]
-}
+	groupDetails: GroupMember[];
+};
 
 const TableRow = (props: TableRowProps) => {
+	const { groupDetails } = props;
 
-    const { groupDetails } = props
+	const groupKeysArray = Object.keys(groupDetails[0]);
 
-    const groupKeysArray = Object.keys(groupDetails[0])
-
-    return (
-        <Fragment>
-                    {groupDetails.map((groupMemberInfo, index)=> {
-                       return ( 
-                       <tr className='bg-blue-200' key={index}>
-                            {groupKeysArray.map((groupKey, keyIndex)=> {
-                                return (
-                                <td key={keyIndex}>
-                                    { typeof groupMemberInfo[groupKey]  === 'string' && groupKey != 'secret_pick' ?
-                                    groupMemberInfo[groupKey] : ( groupKey === 'secret_pick' ? 'pick successful' : 'No pick' )
-                                    }
-                                </td>
-                                )
-                            })}
-                        </tr>
-                       )
-                    })}
-        </Fragment>
-    )
-}
+	return (
+		<Fragment>
+			{groupDetails.map((groupMemberInfo, index) => {
+				return (
+					<tr className='bg-blue-200' key={index}>
+						{groupKeysArray.map((groupKey, keyIndex) => {
+							return (
+								<td key={keyIndex}>
+									{typeof groupMemberInfo[groupKey] === 'string' &&
+									groupKey != 'secret_pick'
+										? groupMemberInfo[groupKey]
+										: groupKey === 'secret_pick'
+										? 'pick successful'
+										: 'No pick'}
+								</td>
+							);
+						})}
+						<button
+							type='button'
+							className='btn-primary mt-2 transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-red-700 hover:bg-red-900 text-white font-normal py-2 px-4 mr-1 rounded'
+						>
+							Edit
+						</button>
+						<button
+							type='button'
+							className='btn-primary mt-2 transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-red-700 hover:bg-red-900 text-white font-normal py-2 px-4 mr-1 rounded'
+						>
+							delete
+						</button>
+					</tr>
+				);
+			})}
+		</Fragment>
+	);
+};
 
 export default TableRow;
