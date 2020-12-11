8824a79647cb9cf19459bb15a17510d9a6c6baf4
James Lovejoy <james.lovejoy2@gmail.com>
2020-12-11 07:20:13 -0600

    added footer to layout.tsx

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navbar } from '../Navbar';
+import { Footer } from '../Footer';
 
 interface LayoutProps {
 	children?: JSX.Element;
@@ -14,6 +15,9 @@ const Layout = props => {
 				<Navbar />
 			</div>
 			<div className='flex grid grid-cols-6 gap-2 justify-center'>{children}</div>
+			<div>
+				<Footer />
+			</div>
 		</>
 	);
 };
