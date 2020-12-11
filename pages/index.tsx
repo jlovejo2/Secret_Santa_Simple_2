039f1b02da98efa1eaeadb04224916ec4980a26a
James Lovejoy <james.lovejoy2@gmail.com>
2020-12-11 06:55:07 -0600

    created home page, and brought hero into it

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,57 +13,19 @@ import {
 	FormEvent
 } from 'react';
 import { Layout } from '../components/Layout';
-
-gql`
-	query Index {
-		allTodos {
-			todoId
-		}
-	}
-`;
+import { Hero } from '../components/UI';
 
 const Index = () => {
-	const { data, loading } = useIndexQuery();
-	const [newTodoDescription, setNewTodoDescription] = useState('');
-
-	const [todoIds, setTodoIds] = useState<string[]>();
-
-	const fillTodoIds = (data: string[]) => {
-		setTodoIds(data?.slice().sort((a, b) => a.localeCompare(b)));
-	};
-
-	useEffect(() => {
-		fillTodoIds(data?.allTodos?.map(t => t.todoId));
-	}, [data?.allTodos]);
-
-	const updateTodoDescription = (e: ChangeEvent) => {
-		setNewTodoDescription((e.target as HTMLInputElement).value.toString());
-	};
-
-	const changeCurrentGroupMember = (e: ChangeEvent) => {};
-
-	const onClickAddTodo = () => {};
-
-	const todoElements = todoIds?.map(id => <Todo todoId={id} key={id} />);
-
-	const body =
-		loading || typeof todoElements === 'undefined' ? null : todoElements.length >
-		  0 ? (
-			<>
-				<table>
-					<tbody>{todoElements}</tbody>
-				</table>
-			</>
-		) : (
-			<div>No ToDos!</div>
-		);
-
 	return (
 		<>
 			<Layout>
-				<div className='col-start-2 col-span-4'>
-					<GroupCombined />
-				</div>
+				<Hero
+					headline={'Welcome to Secret Santa App!'}
+					description={
+						'Create a secret santa group, make you picks with one click of a button, and annonomously send the pick to each group member!'
+					}
+				/>
+				<div className='col-start-2 col-span-4'></div>
 			</Layout>
 		</>
 	);
