3fd9ec24971b1b7dd231687e164b57073f99fbbd
James Lovejoy <james.lovejoy2@gmail.com>
2020-12-12 20:33:05 -0600

    changed color scheme of navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 
 const Navbar = () => {
 	return (
-		<div className='relative bg-white'>
+		<div className='relative bg-red-500'>
 			<div className='max-w-7xl mx-auto px-4 sm:px-6'>
 				<div className='flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10'>
 					<nav className='hidden md:flex space-x-10'>
 						<a
 							href='#'
-							className='text-base font-medium text-gray-500 hover:text-gray-900'
+							className='text-base font-medium text-white hover:text-gray-900'
 						>
 							Secret Santa
 						</a>
 						<a
 							href='/about'
-							className='text-base font-medium text-gray-500 hover:text-gray-900'
+							className='text-base font-medium text-white hover:text-gray-900'
 						>
 							About
 						</a>
@@ -22,13 +22,13 @@ const Navbar = () => {
 					<div className='hidden md:flex items-center justify-end md:flex-1 lg:w-0'>
 						<a
 							href='#'
-							className='whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900'
+							className='whitespace-nowrap text-base font-medium text-white hover:text-gray-900'
 						>
 							Sign in
 						</a>
 						<a
 							href='#'
-							className='ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700'
+							className='ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-green-600 hover:bg-indigo-700'
 						>
 							Sign up
 						</a>
