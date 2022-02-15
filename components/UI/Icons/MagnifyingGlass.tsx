c51851d392e37edf0ce11b6c0575ce67c5822905
Eddie Lovejoy <james.lovejoy2@gmail.com>
2022-02-15 16:23:34 -0500

    moved dashboard svgs into UI/icon folder

---
+++ components/UI/Icons/MagnifyingGlass.tsx
@@ -0,0 +1,24 @@
+const MagnifyingGlass = () => {
+	return (
+		<div className='text-gray-500 absolute ml-4 inset-0 m-auto w-4 h-4'>
+			<svg
+				xmlns='http://www.w3.org/2000/svg'
+				className='icon icon-tabler icon-tabler-search'
+				width={16}
+				height={16}
+				viewBox='0 0 24 24'
+				strokeWidth={1}
+				stroke='#A0AEC0'
+				fill='none'
+				strokeLinecap='round'
+				strokeLinejoin='round'
+			>
+				<path stroke='none' d='M0 0h24v24H0z' />
+				<circle cx={10} cy={10} r={7} />
+				<line x1={21} y1={21} x2={15} y2={15} />
+			</svg>
+		</div>
+	);
+};
+
+export default MagnifyingGlass;
---
+++ components/UI/Icons/index.ts
@@ -0,0 +1,8 @@
+export { default as AlertBellIcon } from './AlertBell';
+export { default as DashboardIcon } from './Dashboard';
+export { default as GithubIcon } from './Github';
+export { default as LogoutIcon } from './Logout';
+export { default as MagnifyingGlassIcon } from './MagnifyingGlass';
+export { default as MessagesIcon } from './Messages';
+export { default as ProfileIcon } from './Profile';
+export { default as RightArrowIcon } from './RightArrow';
