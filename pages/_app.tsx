0782de4031bf31572b7c622fd49c1b4babe6fc34
Eddie Lovejoy <james.lovejoy2@gmail.com>
2022-02-25 15:05:24 -0500

    added headers for authorization to setContext and returned it as link to ApolloClient

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,41 @@
 import {
-    ApolloProvider,
-    ApolloClient,
-    HttpLink,
-    InMemoryCache,
-  } from "@apollo/client";
-  import fetch from "node-fetch"; // Use node-fetch here to allow SSR
-  import '../styles/globals.css'
-  
-  const client = new ApolloClient({
-    link: new HttpLink({ uri: "/api/graphql", fetch: fetch as any }),
-    cache: new InMemoryCache(),
-  });
-  
-  // This default export is required
-  export default function App({ Component, pageProps }) {
-    return (
-      <ApolloProvider client={client}>
-        <Component {...pageProps} />
-      </ApolloProvider>
-    );
-  }
+	ApolloProvider,
+	ApolloClient,
+	HttpLink,
+	InMemoryCache,
+	createHttpLink
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import fetch from 'node-fetch'; // Use node-fetch here to allow SSR
+import '../styles/globals.css';
+
+const httpLink = createHttpLink({
+	uri: '/api/graphql',
+	fetch: fetch as any
+});
+
+const authLink = setContext((_, { headers }) => {
+	const token = localStorage.getItem('auth-token');
+
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `${token}` : ''
+		}
+	};
+});
+
+const client = new ApolloClient({
+	// link: new HttpLink({ uri: "/api/graphql", fetch: fetch as any }),
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache()
+});
+
+// This default export is required
+export default function App({ Component, pageProps }) {
+	return (
+		<ApolloProvider client={client}>
+			<Component {...pageProps} />
+		</ApolloProvider>
+	);
+}
