43d671cc2cc77955c9de9ec502e15c812e74cc77
Eddie Lovejoy <james.lovejoy2@gmail.com>
2022-02-25 15:03:08 -0500

    added hashPassword, generateToken, tradeTokenForUser code.  Also added a decodedToken type

--- src/auth/auth-guard.ts
+++ src/auth/auth-guard.ts
@@ -2,6 +2,6 @@ export const authenticated = next => (root, args, context, info) => {
 	if (!context.currentUser) {
 		throw new Error('Not authenticated');
 	}
-
+	console.log('User Authenticated');
 	return next(root, args, context, info);
 };
--- src/auth/auth-helpers.ts
+++ src/auth/auth-helpers.ts
@@ -1,5 +1,11 @@
 import { User } from '../graphql/types';
 import jwt from 'jsonwebtoken';
+import { genSalt } from 'bcryptjs';
+import { compare, hash } from 'bcrypt';
+
+interface decodedTokenType {
+	[key: string]: any;
+}
 
 export async function tradeTokenForUser(
 	token: string
@@ -8,15 +14,46 @@ export async function tradeTokenForUser(
 
 	if (!verify) return false;
 
-	const decodedToken = jwt.decode(token, { complete: true });
-
+	const decodedToken = jwt.decode(token, { complete: true }) as decodedTokenType;
 	console.log('decode token ', decodedToken);
 
-	return true;
+	return decodedToken.payload.userId;
 }
 
-function generateToken(user) {
-	return jwt.sign(user, process.env.JWT_TOKEN_SECRET, {
+export async function tradeUserForToken(user: User) {
+	const token = await generateToken(user);
+	const verify = await jwt.verify(token, process.env.JWT_TOKEN_SECRET);
+
+	if (verify) return token;
+	else
+		throw new Error('Error when creating token for user, token was not verified');
+}
+
+async function generateToken(user) {
+	return jwt.sign({ userId: user.userId }, process.env.JWT_TOKEN_SECRET, {
 		expiresIn: '120m'
 	});
+}
+
+export async function hashPassword(password: string): Promise<string> {
+	try {
+		const salt = await genSalt(10);
+		const hashedPassword = await hash(password, salt);
+		console.log(salt, hashedPassword);
+		return hashedPassword;
+	} catch (err) {
+		console.error(` Error hashing password: `, err);
+		return password;
+	}
+}
+
+export async function validatePassword(password, hashedPassword) {
+	const isValid = await compare(password, hashedPassword);
+	if (isValid) {
+		console.log('password is valid');
+		return isValid;
+	} else {
+		console.log('password invald');
+		return isValid;
+	}
 }
