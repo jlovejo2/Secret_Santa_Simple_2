4414fabdf6c4ac84b7bb4fa35e0e18d50733482e
Eddie Lovejoy <james.lovejoy2@gmail.com>
2022-03-04 16:27:43 -0500

    added aliases to tscnonfig and next.config for webpack

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
+var path = require('path');
+
 module.exports = {
-    webpack: (config, options) => {
-      config.module.rules.push({
-        test: /\.graphql?$/,
-        loader: "webpack-graphql-loader",
-      });
-  
-      return config;
-    },
-  };
+	webpack: (config, options) => {
+		config.module.rules.push({
+			test: /\.graphql?$/,
+			loader: 'webpack-graphql-loader'
+		});
+
+		// config.resolve.extensions.push('.ts')
+		config.resolve.alias['@components'] = path.join(__dirname, './components');
+		config.resolve.alias['@hooks'] = path.join(__dirname, './hooks');
+		config.resolve.alias['@src'] = path.join(__dirname, './src');
+		config.resolve.alias['@graphql'] = path.join(__dirname, './src/graphql');
+		config.resolve.alias['@dao'] = path.join(__dirname, './src/dao');
+		config.resolve.alias['@auth'] = path.join(__dirname, './src/auth');
+
+		return config;
+	}
+};
