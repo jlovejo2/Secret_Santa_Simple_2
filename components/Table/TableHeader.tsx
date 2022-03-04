4b34cb95b9864425faa62a21eff5d49f9e66b45c
Eddie Lovejoy <james.lovejoy2@gmail.com>
2022-03-04 16:24:26 -0500

    updated table to return string if there is no data submitted to it

--- components/Table/TableHeader.tsx
+++ components/Table/TableHeader.tsx
@@ -2,37 +2,45 @@ import React from 'react';
 import { CreateGroupInput } from '../../src/dao';
 import { GroupMember } from '../../src/graphql/types';
 
-
 type TableHeaderProps = {
-    groupDetails: GroupMember[] | CreateGroupInput[]
-}
+	groupDetails:
+		| GroupMember[]
+		| CreateGroupInput[]
+		| ({ __typename: 'User' } | { __typename: 'NonUser' })[];
+};
 
 const convertToDisplay = (inputString: string) => {
-    const splitArray = inputString.split("_")
-    let displayString = '';
+	const splitArray = inputString.split('_');
+	let displayString = '';
 
-    for (let i of splitArray) {
-        displayString = displayString + ' ' + i
-    }
+	for (let i of splitArray) {
+		displayString = displayString + ' ' + i;
+	}
 
-    return displayString
-}
+	return displayString;
+};
 
 const TableHeader = (props: TableHeaderProps) => {
+	const { groupDetails } = props;
 
-    const { groupDetails } = props
+	const groupKeysElem =
+		groupDetails.length > 0 ? (
+			<thead>
+				<tr>
+					{Object.keys(groupDetails[0]).map((groupKey, index) => {
+						return (
+							<th key={index} className=''>
+								{convertToDisplay(groupKey)}
+							</th>
+						);
+					})}
+				</tr>
+			</thead>
+		) : (
+			<div>No Group members</div>
+		);
 
-    const groupKeysArray = Object.keys(groupDetails[0])
-
-    return (
-        <thead>
-            <tr>
-                {groupKeysArray.map((groupKey, index)=> {
-                    return <th key={index} className=''>{convertToDisplay(groupKey)}</th>
-                })}
-            </tr>
-        </thead>
-    )
-}
+	return <>{groupKeysElem}</>;
+};
 
 export default TableHeader;
--- components/Table/TableRow.tsx
+++ components/Table/TableRow.tsx
@@ -3,53 +3,51 @@ import { GroupMember } from '../../src/graphql/types';
 
 type TableRowProps = {
 	groupDetails: GroupMember[];
-	handleDeleteGroupMember: (e: SyntheticEvent) => void;
+	handleDeleteGroupMember: (index: number) => void;
 };
 
 const TableRow = (props: TableRowProps) => {
 	const { groupDetails, handleDeleteGroupMember } = props;
 
-	const groupKeysArray = Object.keys(groupDetails[0]);
+	const tableRowElem =
+		groupDetails.length > 0 ? (
+			<Fragment>
+				{groupDetails.map((groupMemberInfo, index) => {
+					return (
+						<tr className='bg-blue-200' key={index}>
+							{Object.keys(groupDetails[0]).map((groupKey, keyIndex) => {
+								return (
+									<td key={keyIndex}>
+										{typeof groupMemberInfo[groupKey] === 'string' &&
+										groupKey != 'secret_pick'
+											? groupMemberInfo[groupKey]
+											: groupKey === 'secret_pick'
+											? 'pick successful'
+											: 'No pick'}
+									</td>
+								);
+							})}
+							<a
+								className='btn-primary mt-2 transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-red-700 hover:bg-red-900 text-white font-normal py-2 px-4 mr-1 rounded'
+								// name='groupMemberIndex'
+							>
+								Edit
+							</a>
+							<a
+								className='btn-primary mt-2 transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-red-700 hover:bg-red-900 text-white font-normal py-2 px-4 mr-1 rounded'
+								// name='groupMemberIndex'
+								// value={index}
+								onClick={() => handleDeleteGroupMember(index)}
+							>
+								delete
+							</a>
+						</tr>
+					);
+				})}
+			</Fragment>
+		) : null;
 
-	return (
-		<Fragment>
-			{groupDetails.map((groupMemberInfo, index) => {
-				return (
-					<tr className='bg-blue-200' key={index}>
-						{groupKeysArray.map((groupKey, keyIndex) => {
-							return (
-								<td key={keyIndex}>
-									{typeof groupMemberInfo[groupKey] === 'string' &&
-									groupKey != 'secret_pick'
-										? groupMemberInfo[groupKey]
-										: groupKey === 'secret_pick'
-										? 'pick successful'
-										: 'No pick'}
-								</td>
-							);
-						})}
-						<button
-							type='button'
-							className='btn-primary mt-2 transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-red-700 hover:bg-red-900 text-white font-normal py-2 px-4 mr-1 rounded'
-							name='groupMemberIndex'
-							value={index}
-						>
-							Edit
-						</button>
-						<button
-							type='button'
-							className='btn-primary mt-2 transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-red-700 hover:bg-red-900 text-white font-normal py-2 px-4 mr-1 rounded'
-							name='groupMemberIndex'
-							value={index}
-							onClick={handleDeleteGroupMember}
-						>
-							delete
-						</button>
-					</tr>
-				);
-			})}
-		</Fragment>
-	);
+	return <Fragment>{tableRowElem}</Fragment>;
 };
 
 export default TableRow;
