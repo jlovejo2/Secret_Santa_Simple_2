03bf012a4d36628d679296545fc750790dbfb741
Eddie Lovejoy <james.lovejoy2@gmail.com>
2022-03-10 08:51:35 -0500

    changed modal component so that it can received maxHeight or maxWidth prop but also defaults to a specific size

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,16 +1,31 @@
+import { calculateObjectSize } from 'bson';
 import React, { useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
 import { CloseMenuIcon } from '../UI/Icons';
 import s from './modal.module.scss';
 
-const Modal = ({
-	show,
-	onClose,
-	closeModalInHeader,
-	modalHeader,
-	modalBody,
-	modalFooter
-}) => {
+interface ModalProps {
+	show: Boolean;
+	onClose: () => void;
+	closeModalInHeader: Boolean;
+	modalHeader: JSX.Element;
+	modalBody: JSX.Element;
+	modalFooter: JSX.Element;
+	maxHeight?: String | Number;
+	maxWidth?: String | Number;
+}
+
+const Modal = (props: ModalProps) => {
+	const {
+		show,
+		onClose,
+		closeModalInHeader,
+		modalHeader,
+		modalBody,
+		modalFooter,
+		maxHeight,
+		maxWidth
+	} = props;
 	const [isBrowser, setIsBrowser] = useState(false);
 
 	useEffect(() => {
@@ -22,12 +37,18 @@ const Modal = ({
 		onClose();
 	};
 
+	const style = {
+		'--modal-container-max-height': maxHeight ? maxHeight : '65%',
+		'--modal-container-max-width': maxWidth ? maxWidth : '35%'
+	} as React.CSSProperties;
+
 	const modalContent = show ? (
 		<div
 			className={`${s.modalOverlay} bg-grey-lighter min-h-screen flex flex-col`}
 		>
 			<div
 				className={`${s.modalContainer} container bg-white max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2`}
+				style={style}
 			>
 				<div className={`${s.modalHeader}`}>
 					{modalHeader}
