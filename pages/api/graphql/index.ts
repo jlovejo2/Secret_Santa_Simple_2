47830a029a4e5937f3d9c06994f74d97efd1b01c
Eddie Lovejoy <james.lovejoy2@gmail.com>
2022-03-15 09:44:16 -0400

    moved schema to under the pages/api folder with hopes of resolving issue of accessing schema before intilization in production

--- pages/api/graphql/index.ts
+++ pages/api/graphql/index.ts
@@ -1,12 +1,57 @@
 import { ApolloServer } from 'apollo-server-micro';
 import 'graphql-import-node';
-import { schema } from '@graphql/index';
+// import { schema } from '@graphql/index';
+// import { join } from 'path';
+// import { makeExecutableSchema } from '@graphql-tools/schema';
+// import { loadFilesSync } from '@graphql-tools/load-files';
+// import { mergeTypeDefs, mergeResolvers } from '@graphql-tools/merge';
+// import { DIRECTIVES } from '@graphql-codegen/typescript-mongodb';
+import { tradeTokenForUser } from '@auth/auth-helpers';
 import { join } from 'path';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import { loadFilesSync } from '@graphql-tools/load-files';
 import { mergeTypeDefs, mergeResolvers } from '@graphql-tools/merge';
 import { DIRECTIVES } from '@graphql-codegen/typescript-mongodb';
-import { tradeTokenForUser } from '@auth/auth-helpers';
+// import resolvers from './resolvers';
+import {
+	GroupMutations,
+	GroupQueries
+} from 'pages/api/graphql/schema/group/resolvers';
+import {
+	UserMutations,
+	UserQueries
+} from 'pages/api/graphql/schema/user/resolvers';
+import {
+	TodoMutations,
+	TodoQueries
+} from 'pages/api/graphql/schema/todo/resolvers';
+
+// used process.cwd() here instead of __dirname.  Made it easier for me to visualize the glob pattern to write
+// process.cwd() returns the value of directory where we run the node process
+// __dirname reutrns the value of the directory where the current running file resides
+const loadedTypesFiles = loadFilesSync(
+	join(process.cwd(), './pages/api/graphql/schema/**/*.graphql')
+);
+
+// const loadedResolverFiles = loadFilesSync(
+// 	join(process.cwd(), './src/graphql/**/resolvers/*.ts')
+// );
+
+const typeDefs = mergeTypeDefs(loadedTypesFiles);
+const resolvers = mergeResolvers([
+	GroupMutations,
+	GroupQueries,
+	UserMutations,
+	UserQueries,
+	TodoMutations,
+	TodoQueries
+]);
+
+const schema = makeExecutableSchema({
+	typeDefs: [DIRECTIVES, typeDefs],
+	resolvers
+});
+
 // import resolvers from './resolvers';
 
 // used process.cwd() here instead of __dirname.  Made it easier for me to visualize the glob pattern to write
