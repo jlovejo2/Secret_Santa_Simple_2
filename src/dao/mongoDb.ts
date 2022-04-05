e26f9d8747073fe5af3c1367601227f9e5e405ae
Eddie Lovejoy <james.lovejoy2@gmail.com>
2022-04-05 11:16:51 -0400

    trying to fix issue connecting on deployed code

--- src/dao/mongoDb.ts
+++ src/dao/mongoDb.ts
@@ -1,7 +1,7 @@
 import { MongoClient, Db } from 'mongodb';
 export * from './types';
 
-const { DB_USER, DB_PASSWORD, DB_CLUSTER } = process.env;
+const { DB_USER, DB_PASSWORD, DB_CLUSTER, DB_NAME } = process.env;
 let url: string = '';
 
 if (!DB_USER) {
@@ -10,8 +10,10 @@ if (!DB_USER) {
 	throw new Error('Please define the DB_PASSWORD env variable');
 } else if (!DB_CLUSTER) {
 	throw new Error('Please define the DB_CLUSTER env variable');
+} else if (!DB_NAME) {
+	throw new Error('Please define the DB_NAME env variable');
 } else {
-	url = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}.mongodb.net/<dbname>?retryWrites=true&w=majority`;
+	url = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
 }
 
 export let client: MongoClient;
@@ -20,6 +22,8 @@ export let database: Db;
 export const connect = async (): Promise<Db> => {
 	if (!database) {
 		console.info(`Connecting to database ...`);
+
+		console.log('database url: ', url);
 
 		try {
 			client = await MongoClient.connect(url, {
