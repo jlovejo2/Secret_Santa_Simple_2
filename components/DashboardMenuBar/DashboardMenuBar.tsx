3b74d3b5e81b8cf0270cca2458e59b4c20345b88
Eddie Lovejoy <james.lovejoy2@gmail.com>
2022-04-06 08:46:42 -0400

    added showSearchbar to prop to dashboardMEnubar

--- components/DashboardMenuBar/DashboardMenuBar.tsx
+++ components/DashboardMenuBar/DashboardMenuBar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Searchbar from '../UI/Searchbar';
 import {
 	AlertBellIcon,
 	HamburgerMenuTwoBar,
@@ -11,6 +12,7 @@ import DownArrow from '../UI/Icons/DownArrow';
 
 interface DashboardMenuBarProps {
 	showDashboardMenu: boolean;
+	showSearchbar?: boolean;
 	showProfile: boolean;
 	setShowDashboardMenu: () => void;
 	setShowProfile: () => void;
@@ -20,6 +22,7 @@ const DashboardMenuBar = (props: DashboardMenuBarProps) => {
 	const {
 		showDashboardMenu,
 		showProfile,
+		showSearchbar = true,
 		setShowDashboardMenu,
 		setShowProfile
 	} = props;
@@ -28,16 +31,7 @@ const DashboardMenuBar = (props: DashboardMenuBarProps) => {
 		<nav className='h-16 flex items-center lg:items-stretch justify-end lg:justify-between bg-white shadow relative z-1'>
 			<div className='hidden lg:flex w-full pr-6'>
 				<div className='w-1/2 h-full hidden lg:flex items-center pl-6 pr-24'>
-					<div className='relative w-full'>
-						<div className='text-gray-500 absolute ml-4 inset-0 m-auto w-4 h-4'>
-							<MagnifyingGlassIcon />
-						</div>
-						<input
-							className='border border-gray-100 focus:outline-none focus:border-indigo-700 rounded w-full text-sm text-gray-500 bg-gray-100 pl-12 py-2'
-							type='text'
-							placeholder='Search'
-						/>
-					</div>
+					{showSearchbar ? <Searchbar placeholder='Search' /> : ''}
 				</div>
 				<div className='w-1/2 hidden lg:flex'>
 					<div className='w-full flex items-center pl-8 justify-end'>
