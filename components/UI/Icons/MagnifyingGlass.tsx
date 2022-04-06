d51e98b0f5b9b95965cbb1a3c63839e7f5acecba
Eddie Lovejoy <james.lovejoy2@gmail.com>
2022-04-06 08:45:56 -0400

    added props to magnifying glass

--- components/UI/Icons/MagnifyingGlass.tsx
+++ components/UI/Icons/MagnifyingGlass.tsx
@@ -1,19 +1,31 @@
-const MagnifyingGlass = () => {
+interface MagnifyingGlassProps {
+	width: number;
+	height: number;
+	currentColor?: string;
+}
+
+const MagnifyingGlass = (props: MagnifyingGlassProps) => {
+	const { width, height, currentColor } = props;
+
 	return (
-		<div className='text-gray-500 absolute ml-4 inset-0 m-auto w-4 h-4'>
+		<div>
 			<svg
 				xmlns='http://www.w3.org/2000/svg'
 				className='icon icon-tabler icon-tabler-search'
-				width={16}
-				height={16}
+				width={width}
+				height={height}
 				viewBox='0 0 24 24'
-				strokeWidth={1}
+				strokeWidth={2}
 				stroke='#A0AEC0'
 				fill='none'
 				strokeLinecap='round'
 				strokeLinejoin='round'
 			>
-				<path fill='currentColor' stroke='none' d='M0 0h24v24H0z' />
+				<path
+					fill={currentColor ? 'currentColor' : ''}
+					stroke='none'
+					d='M0 0h24v24H0z'
+				/>
 				<circle cx={10} cy={10} r={7} />
 				<line x1={21} y1={21} x2={15} y2={15} />
 			</svg>
